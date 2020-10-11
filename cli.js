4c7dacb049dfc53fd85ec5d98c8704dfe456b3cd
Dan Jordan <daniel.jordan@mindera.com>
2020-10-11 14:11:28 +0100

    Update dependencies and card border

--- cli.js
+++ cli.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 const boxen = require("boxen");
-const leftPad = require("left-pad");
 
 let output = `Dan Jordan / danjordan
 
-${leftPad("Work:", 12)}  Frontend Developer at Mindera
-${leftPad("Twitter:", 12)}  https://twitter.com/danjordan
-${leftPad("Github:", 12)}  https://github.com/danjordan
-${leftPad("Web:", 12)}  https://dan-jordan.co.uk`;
+${"Work:".padStart(12)} Frontend Developer at Mindera
+${"Twitter:".padStart(12)} https://twitter.com/danjordan
+${"Github:".padStart(12)} https://github.com/danjordan
+${"Web:".padStart(12)} https://www.danjordan.dev`;
 
 console.log(
   boxen(output, {
-    borderStyle: "round",
-    padding: 1
+    borderStyle: "bold",
+    padding: 1,
   })
 );
