d5fec697e51f57222cabc0f77bf9626faee42708
Dan Jordan <daniel.jordan@mindera.com>
2024-06-17 15:52:23 +0100

    Update deps

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-const boxen = require("boxen");
+import boxen from "boxen";
 
 let output = `Dan Jordan / danjordan
 
-${"Work:".padStart(12)} Frontend Developer at Mindera
+${"Work:".padStart(12)} Tech Lead at Mindera
 ${"Twitter:".padStart(12)} https://twitter.com/danjordan
 ${"Github:".padStart(12)} https://github.com/danjordan
 ${"Web:".padStart(12)} https://www.danjordan.dev`;
